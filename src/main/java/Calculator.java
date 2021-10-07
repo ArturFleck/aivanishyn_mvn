7ea936ccabb2f22e8045cb89ae55b57808eeb91b
ArturFleck <antonio.bond2013@gmail.com>
2021-10-07 20:51:35 +0300

    lesson 16 some

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,11 @@ public class Calculator {
     public static int sum(int a, int b){
         return a+b;
     }
+    public static int mult(int a, int b){
+        return a*b;
+    }
+    public static double div(int a, int b){
+        return a/b;
+    }
+
 }
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -3,8 +3,23 @@ import org.junit.jupiter.api.Test;
 
 public class TestCalculator {
 
-@Test
-    public void test_sum_normal_case(){
-    Assertions.assertEquals(4,Calculator.sum(3,2));
+    @Test
+    public void test_sum_normal_case() {
+        Assertions.assertEquals(5, Calculator.sum(3, 2));
+    }
+
+    @Test
+    public void test_mult_normal_case() {
+        Assertions.assertEquals(6, Calculator.mult(3, 2));
+    }
+
+    @Test
+    public void test_div_normal_case() {
+        Assertions.assertEquals(2, Calculator.div(4, 2));
+    }
+
+    @Test
+    public void test_division_by_zero(){
+        Assertions.assertThrows(ArithmeticException.class, ()-> Calculator.div(4, 0) );
     }
 }
