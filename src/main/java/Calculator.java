8448f54cd239231531f43e0f27d872f5a6079280
ArturFleck <Amatory6!>
2021-10-10 15:21:46 +0300

    Homework_16 start

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,5 +8,4 @@ public class Calculator {
     public static double div(int a, int b){
         return a/b;
     }
-
 }
---
+++ src/test/java/Homework_16/TestStringCollectionImpl.java
@@ -0,0 +1,10 @@
+package Homework_16;
+
+import org.junit.jupiter.api.Test;
+
+public class TestStringCollectionImpl {
+    @Test
+    public void test_get(){
+
+    }
+}
