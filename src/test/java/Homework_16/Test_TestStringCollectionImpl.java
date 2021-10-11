fceffea13ece3a11d19e149d67c6114a94f4019a
ArturFleck <antonio.bond2013@gmail.com>
2021-10-11 20:23:31 +0300

    Homework_16 start2

--- src/test/java/Homework_16/Test_TestStringCollectionImpl.java
+++ src/test/java/Homework_16/Test_TestStringCollectionImpl.java
@@ -13,5 +13,21 @@ public class Test_TestStringCollectionImpl {
         Assertions.assertEquals("two",data.get(1));
     }
 
+    @Test
+    public void test_add(){
+        StringCollectionImpl data = new StringCollectionImpl();
+        String str = "Some Text";
+        Assertions.assertEquals(data.add(str), data.contains(str));
+    }
+
+    @Test
+    public void test_addByIndex(){
+        StringCollectionImpl data = new StringCollectionImpl();
+        data.add("one");
+        data.add("two");
+        String str = "Some Text";
+        Assertions.assertEquals(data.add(1,str), data.contains(str));
+    }
+
 
 }
