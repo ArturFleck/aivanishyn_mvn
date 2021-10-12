cf319582d62fc99beb79f8dd83007c249e0fd3fa
ArturFleck <artur.ivanishyn@gmail.com>
2021-10-12 16:44:51 +0300

    Homework_16 going

--- src/test/java/Homework_16/Test_TestStringCollectionImpl.java
+++ src/test/java/Homework_16/Test_TestStringCollectionImpl.java
@@ -2,6 +2,7 @@ package Homework_16;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class Test_TestStringCollectionImpl {
 
@@ -15,9 +16,16 @@ public class Test_TestStringCollectionImpl {
 
     @Test
     public void test_add(){
-        StringCollectionImpl data = new StringCollectionImpl();
+        //StringCollectionImpl data = new StringCollectionImpl();
+        StringCollectionImpl data = Mockito.mock(StringCollectionImpl.class);
+        data.add("one");
+        data.add("two");
         String str = "Some Text";
-        Assertions.assertEquals(data.add(str), data.contains(str));
+        data.add(str);
+        System.out.println(data.size());
+        Mockito.when(data.get(2)).thenReturn(str);
+        Assertions.assertEquals(str, data.get(2));
+        Assertions.assertEquals(3,data.size());
     }
 
     @Test
