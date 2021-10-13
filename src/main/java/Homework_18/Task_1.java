ace541e6a8f905a12dc7255bf6afbde3a658dcfe
Artur Ivanishyn <artur.ivanishyn@gmail.com>
2021-10-13 22:46:34 +0300

    Homework_18 start

--- src/main/java/Homework_18/Task_1.java
+++ src/main/java/Homework_18/Task_1.java
@@ -7,7 +7,7 @@ public class Task_1 {
     public static void main(String[] args) {
         List<Integer> intList = new ArrayList<>();
         int sizeOfArray = 100;
-        for (int i = 0; i < sizeOfArray; i++) intList.add(RandomGenerator.getIntValue(100));
+        for (int i = 0; i < sizeOfArray; i++) intList.add(RandomGenerator.getIntValue(999));
         System.out.println(intList);
         System.out.println("Average of Array is: " + intList.stream().mapToInt(a ->a).average().orElse(-1));
     }
