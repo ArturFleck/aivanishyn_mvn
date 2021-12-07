26af32b19dbf67520ce6f87ebd12734372d52a10
Artur Ivanishyn <artur.ivanishyn@gmail.com>
2021-12-07 10:56:08 +0200

    hw_27 fixes

--- src/main/java/homework_27/StudentService.java
+++ src/main/java/homework_27/StudentService.java
@@ -4,7 +4,6 @@ import java.sql.*;
 
 public class StudentService {
 
-
     Students getStudentByID(int id) throws SQLException {
         Connection connection = null;
         Statement statement =null;
@@ -15,7 +14,7 @@ public class StudentService {
             connection= ConnectionUtil.getConnection();
             statement= connection.createStatement();
             ResultSet rs = statement.executeQuery(sql);
-            student = getData(rs, student);   // going to DB
+            student = getData(rs);   // going to DB
         }catch (SQLException throwables){
             throwables.printStackTrace();
         }finally {
@@ -34,17 +33,36 @@ public class StudentService {
     }
 
     Students getStudenByLastName(String lastName) throws SQLException {
-        Connection connection = ConnectionUtil.getConnection();
-        Statement st = connection.createStatement();
-        ResultSet rs = st.executeQuery("select * from students where last_name='" + lastName + "'");
-        Students stud = null;
-        stud = getData(rs, stud);   // going to DB
-        connection.close();
-        return stud;
+        Connection connection = null;
+        Statement statement =null;
+        Students student = null;
+
+        String sql = "select * from students where last_name='" + lastName + "'";
+        try{
+            connection= ConnectionUtil.getConnection();
+            statement= connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            student = getData(rs);   // going to DB
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }finally {
+            try {
+                if (statement!=null){
+                    statement.close();
+                }
+                if (connection!=null){
+                    connection.close();
+                }
+            }catch (SQLException sqlException){
+                sqlException.printStackTrace();
+            }
+        }
+        return student;
     }
 
     // take from DB what needed at resultSet
-    private Students getData(ResultSet rs, Students stud) throws SQLException {
+    private Students getData(ResultSet rs ) throws SQLException {
+        Students stud = null;
         while (rs.next()) {
             int id = rs.getInt("id");
             String firstName = rs.getString("first_name");
