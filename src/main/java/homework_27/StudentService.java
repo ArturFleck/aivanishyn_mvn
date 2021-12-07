361af421b5eba822ce55ea582751f48f20feb596
Artur Ivanishyn <artur.ivanishyn@gmail.com>
2021-12-07 10:51:46 +0200

    hw_27 fixes

--- src/main/java/homework_27/StudentService.java
+++ src/main/java/homework_27/StudentService.java
@@ -6,13 +6,31 @@ public class StudentService {
 
 
     Students getStudentByID(int id) throws SQLException {
-        Connection connection = ConnectionUtil.getConnection();
-        Statement st = connection.createStatement();
-        ResultSet rs = st.executeQuery("select * from students where id=" + id);
-        Students stud = null;
-        stud = getData(rs, stud);   // going to DB
-        connection.close();
-        return stud;
+        Connection connection = null;
+        Statement statement =null;
+        Students student = null;
+
+        String sql = "select * from students where id=" + id;
+        try{
+            connection= ConnectionUtil.getConnection();
+            statement= connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            student = getData(rs, student);   // going to DB
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }finally {
+            try {
+                if (statement!=null){
+                    statement.close();
+                }
+                if (connection!=null){
+                    connection.close();
+                }
+            }catch (SQLException sqlException){
+                sqlException.printStackTrace();
+            }
+        }
+        return student;
     }
 
     Students getStudenByLastName(String lastName) throws SQLException {
